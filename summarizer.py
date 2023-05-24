3d90d79c319b8f8d0ec61ab1f0db43928d86d04c
clarkdever <clarkdever@gmail.com>
2023-05-24 15:02:12 -0500

    Preprocess transcripts to strip out timestamps -Reduces token cost and number of chunks Add inputs and results to .gitignore

--- summarizer.py
+++ summarizer.py
@@ -5,6 +5,7 @@ import json
 import threading
 from dotenv import load_dotenv
 import glob
+import re
 
 # Load .env file
 load_dotenv()
@@ -27,7 +28,6 @@ def split_text(text, word_limit=1300):
     num_parts = len(words) // word_limit + 1
     return [' '.join(words[i * word_limit: (i + 1) * word_limit]) for i in range(num_parts)]
 
-
 def print_period():
     """
     Prints a period character to the console every second. 
@@ -37,7 +37,6 @@ def print_period():
     print(".", end="", flush=True)
     period_timer = threading.Timer(1.0, print_period)
     period_timer.start()
-
 
 def load_prompt(filename="prompt.json"):
     """
@@ -52,6 +51,10 @@ def load_prompt(filename="prompt.json"):
     with open(filename, "r") as f:
         return json.load(f)
 
+def preprocess_text(text):
+    lines = text.split('\n')
+    processed_lines = [line for line in lines if not line.strip().startswith(tuple('0123456789')) and line.strip() != '']
+    return '\n'.join(processed_lines)
 
 def create_summary(file_path, prompt):
     """
@@ -68,16 +71,19 @@ def create_summary(file_path, prompt):
         print(f"Reading {os.path.basename(file_path)}")
         transcript = f.read()
 
+    # Preprocess the transcript
+    transcript = preprocess_text(transcript)
+
     chunks = split_text(transcript)
     summary_list = []
+    total_chunks = len(chunks)
 
-    for i, chunk in enumerate(chunks):
+    for i, chunk in enumerate(chunks, start=1):
         prompt[-2]["content"] = chunk
         retries = 0
-
         while retries < 5:
             try:
-                print(f"Making summarization request to OpenAI API for {os.path.basename(file_path)}", end="")
+                print(f"Making summarization request {i}/{total_chunks} to OpenAI API for {os.path.basename(file_path)}", end="")
                 print_period()
 
                 response = openai.ChatCompletion.create(
@@ -88,29 +94,23 @@ def create_summary(file_path, prompt):
                     n=1,
                     stop=None
                 )
-
                 period_timer.cancel()
                 print("")
                 summary = response.choices[0].message['content'].strip()
                 summary_list.append(summary)
                 break
-
             except openai.error.RateLimitError:
                 print("\nReceived rate limit error, waiting for 60 seconds before retrying...")
                 period_timer.cancel()
                 time.sleep(60)
                 retries += 1
-
             except Exception as e:
                 print("\nQuitting due to unexpected error: {}".format(e))
                 period_timer.cancel()
                 return None
-
         if retries >= 5:
             print("\nExceeded maximum number of retries. Giving up on current chunk.")
-    
     return '\n'.join(summary_list)
-
 
 def save_summary(file_path, summary):
     """
@@ -135,13 +135,10 @@ def process_files(input_files, prompt):
     """
     for file_path in input_files:
         output_file = os.path.join("results", os.path.basename(file_path).replace(".txt", "-summary.txt"))
-
         if os.path.exists(output_file):
             print(f"Output file {os.path.basename(output_file)} already exists, skipping.")
             continue
-
         summary = create_summary(file_path, prompt)
-
         if summary is not None:
             try:
                 save_summary(output_file, summary)
@@ -151,7 +148,6 @@ def process_files(input_files, prompt):
                 print(f"File already exists: {output_file}")
             except Exception as e:
                 print(f"An error occurred while writing the file: {output_file}. Error: {e}")
-
 
 # Define the timer object as global so we can access it everywhere
 period_timer = None
